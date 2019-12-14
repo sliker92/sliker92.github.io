705e8823473afc74c1033e1cd30c593d69ceee2d
sliker92 <47948983+sliker92@users.noreply.github.com>
2019-12-14 23:25:23 +0300

    added Macbook's iPad's

--- maps.js
+++ maps.js
@@ -1,7 +1,14 @@
 var map;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 55.755733, lng: 37.609544 },
     zoom: 17
   });
+  var myMap = document.getElementById('map');
+  var modal = document.querySelector('.map-modal-window');
+  var newModal = modal.cloneNode(true);
+  newModal.className = 'map-modal-window';
+  myMap.appendChild(newModal);
+  console.log(newModal);
 }
